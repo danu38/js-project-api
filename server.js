a8a83899e6bfd3c14d7a8acc1149d6d066df3430
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-06-17 09:00:34 +0200

    file initiated

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-import cors from "cors"
-import express from "express"
+import cors from "cors";
+import express from "express";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
+let thoughts = JSON.parse(fs.readFileSync('./data.json'));
+
+// Start defining routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
-})
+  res.send("Welcome to the Happy Thoughts API 💬");
+});
+
+app.get("/thoughts", (req, res) => {
+  res.json([
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" },
+  ]);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
