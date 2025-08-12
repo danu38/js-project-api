6ac4e9040d07eed5a5d2d8463f89a361f0155d85
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-08-12 08:58:42 +0200

    coonection build with mongodb

--- server.js
+++ server.js
@@ -2,18 +2,39 @@ import cors from "cors";
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import fs from "fs";
+import mongoose from 'mongoose';
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-let thoughts = JSON.parse(fs.readFileSync('./data.json'));       
+let thoughts = JSON.parse(fs.readFileSync('./data.json'));     
+
+
+
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+mongoose.connect(mongoUrl)
+mongoose.Promise = Promise
+
+const Author = mongoose.model('Author', {
+  name: String
+})
+
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+})
+
 
 // Start defining routes here
 app.get("/", (req, res) => {
@@ -69,3 +90,6 @@ app.get("/thoughts/:id", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
