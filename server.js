aa0ec2266e1407b0b636a3f95f85c52992631979
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-09-04 15:33:37 +0200

    cors error fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors({
-  origin: ["https://happythoughtsappbydanu.netlify.app/"], // add your deployed FE origin here later
+  origin: ["http://localhost:5174", "https://happythoughtsappbydanu.netlify.app"], // add your deployed FE origin here later
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   // credentials: true, // only if you ever use cookies
